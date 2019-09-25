31aa9682f4aff6249310682c6159dd011b29a5f0
etienne <wvetienne@gmail.com>
2019-09-26 00:59:44 +0200

    lala

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(router)
 
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static('client/build'))
+    app.get('*',(req, res) =>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    })
+}
+
 app.get('', function (req, res) {
     res.send("I'm the tiennelord API");
 });
