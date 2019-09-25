49b2720eb7f40cf52a411e17f15975dba2b6d7a3
etienne <wvetienne@gmail.com>
2019-09-25 17:24:15 +0200

    pb procfile

--- server.js
+++ server.js
@@ -11,28 +11,15 @@ port = process.env.PORT || 5000;
 
 
 app.use(logger('dev'))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*')
-    // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(router)
 
 app.get('', function (req, res) {
-    
     res.send("I'm the tiennelord API");
 });
 
 app.listen( port, () => {
     console.log("Runnning on " + port)
 })
-
-module.exports = function(app) {
-    app.use(
-      proxy(["/api", , "/otherApi"], { target: "http://localhost:5000" })
-    );
-  }
